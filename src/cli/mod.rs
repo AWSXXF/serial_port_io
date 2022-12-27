d7df073396d6f372438779e0a852ba6ccd6f7b4e
ydd <ydd_180@163.com>
2022-12-28 04:18:39 +0800

    [feature]   Cross-platform support [body]      1. 跨平台支持             2. 将默认格式改为utf-8

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -10,9 +10,11 @@ use clap::Parser;
 pub struct Cli {
     /// enable utf-8 encoding mode, default: GB2312
     #[arg(long)]
-    pub utf8: bool,
+    pub gb2312: bool,
 
-    /// serial name like `com10`
-    #[arg(required = true)]
-    pub com_name: String,
+    // #[arg(short, long, value_name = "list", action)]
+    // pub list_port: bool,
+    /// serial port name like `com10`
+    #[arg(required = false, value_name = "serial port")]
+    pub port_name: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use serial_io::{bindings::c_getch, cli, Access, SerialPort};
+use serial_io::{bindings::c_getch, cli};
+use std::io::ErrorKind::TimedOut;
 use std::io::{Read, Result, Write};
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -7,35 +8,56 @@ use std::time::Duration;
 use textcode::gb2312;
 
 fn main() -> Result<()> {
+    // if cfg!(target_os = "windows") {
+    //     std::process::Command::new("chcp")
+    //         .env("PATH", std::env!("PATH"))
+    //         .arg("65001")
+    //         .output()
+    //         .unwrap();
+    // }
+
     // 命令行参数
     ctrlc::set_handler(|| {}).expect("无法忽略SIGINT信号");
     let args = cli::Cli::parse();
-    let com = args.com_name;
-    let use_utf8 = args.utf8;
+    let port_name = args.port_name;
+    let use_gb2312 = args.gb2312;
+    // let list_port = args.list_port;
+
+    // if list_port == true {
+    //     let ports = serialport::available_ports().expect("No ports found!");
+    //     for p in ports {
+    //         println!("{}({:?})", p.port_name, p.port_type);
+    //     }
+    //     return Ok(());
+    // }
 
     // 初始化
-    let display = if use_utf8 == true {
-        |buf: &[u8]| {
-            print!("{}", String::from_utf8(buf.to_vec()).unwrap());
-            std::io::stdout().flush().unwrap();
-        }
-    } else {
+    let display = if use_gb2312 == true {
         |buf: &[u8]| {
             print!("{}", gb2312::decode_to_string(buf));
             std::io::stdout().flush().unwrap();
         }
+    } else {
+        |buf: &[u8]| {
+            print!("{}", String::from_utf8(buf.to_vec()).unwrap());
+            std::io::stdout().flush().unwrap();
+        }
     };
     let stop = Arc::new(Mutex::new(false));
-    let mut sp = SerialPort::try_new(format!("\\\\.\\{}", com).as_str(), Access::All).unwrap();
-    sp.init().unwrap();
-    let sp = Arc::new(Mutex::new(sp));
+
+    let port = serialport::new(port_name, 115_200)
+        .timeout(Duration::from_millis(100))
+        .open()
+        .expect("Failed to open port");
+
+    let port = Arc::new(Mutex::new(port));
 
     // 接受线程
     let recive_thread = {
         let stop = stop.clone();
-        let sp = sp.clone();
+        let port = port.clone();
         thread::spawn(move || {
-            let mut buf: [u8; 255] = [0; 255];
+            let mut serial_buf: Vec<u8> = vec![0; 32];
             let mut size = 0;
             let _ = size; // just make compiler happy
 
@@ -45,11 +67,17 @@ fn main() -> Result<()> {
                 }
 
                 {
-                    let mut sp = sp.lock().unwrap();
-                    size = sp.read(&mut buf).unwrap();
+                    let mut port = port.lock().unwrap();
+
+                    size = match port.read(serial_buf.as_mut_slice()) {
+                        Ok(ret) => ret,
+                        Err(err) if err.kind() == TimedOut => 0,
+                        Err(err) => panic!("serial read error: {}", err.to_string()),
+                    };
                 }
+
                 if size != 0 {
-                    display(&buf[0..size]);
+                    display(&serial_buf[0..size]);
                 } else {
                     thread::sleep(Duration::from_micros(10));
                 }
@@ -60,7 +88,7 @@ fn main() -> Result<()> {
     // 发送线程
     let write_thread = {
         let stop = stop.clone();
-        let sp = sp.clone();
+        let port = port.clone();
         thread::spawn(move || {
             let mut ch = [0u8; 1];
             loop {
@@ -73,8 +101,8 @@ fn main() -> Result<()> {
                     break;
                 }
                 {
-                    let mut sp = sp.lock().unwrap();
-                    sp.write(&ch).unwrap();
+                    let mut port = port.lock().unwrap();
+                    port.write(&ch).expect("Write failed!");
                 }
             }
         })
